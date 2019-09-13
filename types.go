4b7254e9c7411b229a57ff7830292b6cd46769a6
Jonathan Santos Moura <jonathanmsantosmoura@gmail.com>
2019-09-13 16:05:59 -0300

    add xmlns KeyInfo

--- types.go
+++ types.go
@@ -45,7 +45,7 @@ type Transforms struct {
 
 // KeyInfo is an optional element that enables the recipient(s) to obtain the key needed to validate the signature.
 type KeyInfo struct {
-	XMLName  xml.Name `xml:"KeyInfo"`
+	XMLName  xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# KeyInfo"`
 	X509Data *X509Data
 	Children []interface{}
 }
